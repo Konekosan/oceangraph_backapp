93475b1d608e59097ba2fade4c08eaca76f2599f
Konekosan <benjamin.parageaud@viacesi.fr>
2024-09-11 14:31:33 +0200

    add delete options boards

--- app/controller/planche_repository.py
+++ app/controller/planche_repository.py
@@ -13,9 +13,13 @@ def _print(text: str, log_level='info'):
     print(text)
     getattr(loggers, log_level)(text)
 
-# Get planche
-def get_planches(db:Session, skipt:int=0, limit:int=100):
+# Get all planches
+def fetch_all_planches(db:Session, skipt:int=0, limit:int=100):
     return db.query(Planche).offset(skipt).limit(limit).all()
+
+# Get planche by id
+def fetch_planche_by_id(db:Session, planche_id: int):
+    return db.query(Planche).filter(Planche.id == planche_id).first()
 
 # Create planche
 def add_planche(db:Session, planche: PlancheSchema):
@@ -23,7 +27,6 @@ def add_planche(db:Session, planche: PlancheSchema):
                    name=planche.name, 
                    brand=planche.brand, 
                    shape=planche.shape)
-    print(_planche)
     db.add(_planche)
     try:
         db.commit()
@@ -35,3 +38,16 @@ def add_planche(db:Session, planche: PlancheSchema):
             detail="Erreur lors de l'ajout",
         )
     return _planche
+
+def delete_planche(db:Session, planche: PlancheSchema):
+    _planche = fetch_planche_by_id(db, planche)
+
+    try:
+        db.delete(_planche)
+        _print('Planche supprimée avec succès ! ')
+        db.commit()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Erreur lors de la supression",
+        )    
--- app/view/planche_api.py
+++ app/view/planche_api.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter, Depends
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
@@ -5,20 +7,29 @@ from app.config.database import get_db
 from app.controller import planche_repository
 from app.schema.planche_schema import RequestPlancheSchema
 
+loggers = logging.getLogger(__name__)
+
 planche_router = APIRouter()
+
+def _print(text: str, log_level='info'):
+    print(text)
+    getattr(loggers, log_level)(text)
 
 # Fetch all planches
 @planche_router.get("/")
 async def get(db:Session=Depends(get_db)):
-    # _planches = 'coucou'
-    _planches = planche_repository.get_planches(db, 0, 100)
+    _planches = planche_repository.fetch_all_planches(db, 0, 100)
     return _planches, 200
 
 # Creation d'une planche
 @planche_router.post("/add")
 async def create(request: RequestPlancheSchema, db:Session=Depends(get_db)):
-
-    print('aaaaaaaaaa')
-    print(request.parameter)
     _planche = planche_repository.add_planche(db, request.parameter)
     return _planche, 200
+
+# Delete usager by id
+@planche_router.delete("/{id}")
+def delete(id: int, db:Session=Depends(get_db)):
+    planche_repository.delete_planche(db, id)
+    message = 'Planche ' + str(id) + ' supprimée avec succès'
+    return {message, 200}
